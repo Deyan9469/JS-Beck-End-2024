268cb8cfabb75eb6375d154ed7928d25c6997e37
Deyan Yosifov <deqnpavlikov@gmail.com>
2024-01-25 13:16:37 +0200

    add details page

--- Movie-Magic-Workshop/src/controllers/movieController.js
+++ Movie-Magic-Workshop/src/controllers/movieController.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
 const movieService = require('../services/movieService');
 
-router.get('/create', (req,res) => {
+router.get('/create', (req, res) => {
     res.render('create');
 });
-router.post('/create', (req,res) => {
+router.post('/create', (req, res) => {
     const newMovie = req.body;
 
     movieService.create(newMovie);
 
     res.redirect('/');
 });
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
+
+    res.render('details', { movie });
+})
 
 module.exports = router;
--- Movie-Magic-Workshop/src/services/movieService.js
+++ Movie-Magic-Workshop/src/services/movieService.js
@@ -12,6 +12,11 @@ const movies = [{
 exports.getAll = () => {
   return movies.slice();
 }
+exports.getOne = (movieId) => {
+  const movie = movies.find(movie => movie._id === Number(movieId));
+
+  return movie;
+}
 
 exports.create = (movieData) => {
   movieData._id = movies[movies.length - 1]._id + 1;
