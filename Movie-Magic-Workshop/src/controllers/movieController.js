0bb4a6f29cf5952e8bfc0f0560d60543d40915f1
Deyan Yosifov <deqnpavlikov@gmail.com>
2024-01-30 20:13:36 +0200

    show movie data on attach page

--- Movie-Magic-Workshop/src/controllers/movieController.js
+++ Movie-Magic-Workshop/src/controllers/movieController.js
@@ -26,9 +26,10 @@ router.get('/movies/:movieId', async (req, res) => {
     res.render('details', { movie });
 });
 
-router.get('/movies/:movieId/attach', (req, res) => {
+router.get('/movies/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
 
-    res.render('movie/attach');
+    res.render('movie/attach', { ...movie });
 
 });
 
